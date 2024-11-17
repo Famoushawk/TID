e0ab9b2378005f42da40b1213413b6141d2005a8
michaelazis <ziskomichaela@gmail.com>
2024-11-17 16:46:50 +0100

    Frame 2 renamed

---
+++ tid_project/src/pages/ProfileList/ProfileList.js
@@ -0,0 +1,13 @@
+import { WhiteBackground } from '../../components/layout/Layout.styles';
+import React from 'react';
+import Profile from './Profile';
+
+const ProfileList = () => {
+  return (
+    <WhiteBackground>
+      <Profile/>
+    </WhiteBackground>
+  );
+};
+
+export default ProfileList;
