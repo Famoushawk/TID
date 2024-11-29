aadcb8da76311ba075556676ae49fb67ad7d54f7
Christoffer Høeg <clh@northarc.dk>
2024-11-29 11:56:13 +0100

    useEffect import

--- tid_project/src/pages/ProfileList/Profile.js
+++ tid_project/src/pages/ProfileList/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import ProfileOptions from "./ProfileOptions";
 import { AuthService } from '../../services/AuthService';
