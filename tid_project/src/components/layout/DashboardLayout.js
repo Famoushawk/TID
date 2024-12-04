366ce7a33c52d3c779a894500fd2fd8efc52e048
cecm <cecm@itu.dk>
2024-12-04 15:12:17 +0100

    changed frame names

--- tid_project/src/components/layout/DashboardLayout.js
+++ tid_project/src/components/layout/DashboardLayout.js
@@ -13,6 +13,8 @@ import {
   MainContent
 } from './Layout.styles';
 
+//Lets consider hidding this functionality under the burger menu
+
 const DashboardLayout = () => {
   const [isNewThreadDialogOpen, setIsNewThreadDialogOpen] = useState(false);
   const navigate = useNavigate();
@@ -32,12 +34,12 @@ const DashboardLayout = () => {
       <DashboardContainer>
         <NavBar>
           <NavContent>
-            <NavTitle>Dashboard</NavTitle>
+            <NavTitle>Dashboard</NavTitle> 
             <NavLinks>
-              <Link to="/frame1" component={StyledNavLink}>Frame 1</Link>
-              <Link to="/frame2" component={StyledNavLink}>Frame 2</Link>
-              <Link to="/frame3" component={StyledNavLink}>Frame 3</Link>
-              <Link to="/frame4" component={StyledNavLink}>Frame 4</Link>
+              <Link to="/frame1" component={StyledNavLink}>Your overview</Link>
+              <Link to="/frame2" component={StyledNavLink}>Frame2</Link>
+              <Link to="/frame3" component={StyledNavLink}>Content</Link>
+              <Link to="/frame4" component={StyledNavLink}>Debate</Link>
               <StyledNavLink as="button" onClick={handleLogout}>
                 Logout
               </StyledNavLink>
